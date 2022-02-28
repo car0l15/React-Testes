5804c260f281b094de166132a175378b608c718f
Caroline Nepomuceno <carol14898@gmail.com>
2022-02-28 11:43:17 -0300

    comentando pra nao dar erro no lint

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import renderWithRouter from './renderWithRouter';
+// import Pokemon from '../components/Pokemon';
+// import App from '../App';
+// import pokemon from '../data';
+
+// describe('Verifica se o card pokemon mostra essas informações', () => {
+//   test('testando se o nome dos componentes aparece corretamente', () => {
+
+//   });
+// });
